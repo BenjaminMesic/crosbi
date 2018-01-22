27c476760ecfa49fa9a2f7da1db5cf59ad32a8c6
BenjaminMesic <benjamin.mesic@cern.ch>
2018-01-22 18:34:34 +0100

    Improved readme.md

--- _1_prepare_input.py
+++ _1_prepare_input.py
@@ -20,249 +20,249 @@ from cStringIO import StringIO
 output_file = 'list_of_CROSBI_input.json'
 
 # If list empty run over all papers, else run over the ones which are in the list
-eprint 		= [] # e.g. '1509.03750'
+eprint    = [] # e.g. '1509.03750'
 
 authors = {
-	'N. Godinovic'				: 'Godinovi\xc4\x87, Nikola',
-	'D. Lelas' 						: 'Lelas, Damir',
-	'I. Puljak' 					: 'Puljak, Ivica',					
-	'Z. Antunovic' 				: 'Antunovi\xc4\x87, \xc5\xbdeljko',
-	'M. Kovac' 						: 'Kova\xc4\x8d, Marko',
-	'V. Brigljevic' 			: 'Brigljevi\xc4\x87, Vuko',
-	'K. Kadija' 					: 'Kadija, Kre\xc5\xa1o',
-	'J. Luetic' 					: 'Lueti\xc4\x87, Jelena',
-	'S. Micanovic' 				: 'Mi\xc4\x87anovi\xc4\x87, Sa\xc5\xa1a',
-	'L. Sudic' 						: 'Sudi\xc4\x87, Lucija',
-	'T. Susa' 						: '\xc5\xa0u\xc5\xa1a, Tatjana',
-	'D. Polic' 						: 'Poli\xc4\x87, Dunja',
-	'S. Morovic' 					: 'Morovi\xc4\x87, Sre\xc4\x87ko',
-	'L. Tikvica' 					: 'Tikvica, Lucija',
-	'D. Mekterovic' 			: 'Mekterovi\xc4\x87, Darko',
-	'D. Ferencek'					: 'Feren\xc4\x8dek, Dinko',
-	'A. Starodumov' 			: 'Starodumov, Andrey',
-	'B. Mesic' 				 		: 'Mesi\xc4\x87, Benjamin',
-	'S. Duric' 				 		: 'Duri\xc4\x87, Senka',	
+  'N. Godinovic'        : 'Godinovi\xc4\x87, Nikola',
+  'D. Lelas'            : 'Lelas, Damir',
+  'I. Puljak'           : 'Puljak, Ivica',          
+  'Z. Antunovic'        : 'Antunovi\xc4\x87, \xc5\xbdeljko',
+  'M. Kovac'            : 'Kova\xc4\x8d, Marko',
+  'V. Brigljevic'       : 'Brigljevi\xc4\x87, Vuko',
+  'K. Kadija'           : 'Kadija, Kre\xc5\xa1o',
+  'J. Luetic'           : 'Lueti\xc4\x87, Jelena',
+  'S. Micanovic'        : 'Mi\xc4\x87anovi\xc4\x87, Sa\xc5\xa1a',
+  'L. Sudic'            : 'Sudi\xc4\x87, Lucija',
+  'T. Susa'             : '\xc5\xa0u\xc5\xa1a, Tatjana',
+  'D. Polic'            : 'Poli\xc4\x87, Dunja',
+  'S. Morovic'          : 'Morovi\xc4\x87, Sre\xc4\x87ko',
+  'L. Tikvica'          : 'Tikvica, Lucija',
+  'D. Mekterovic'       : 'Mekterovi\xc4\x87, Darko',
+  'D. Ferencek'         : 'Feren\xc4\x8dek, Dinko',
+  'A. Starodumov'       : 'Starodumov, Andrey',
+  'B. Mesic'            : 'Mesi\xc4\x87, Benjamin',
+  'S. Duric'            : 'Duri\xc4\x87, Senka',  
 }
 
 first_author = {
-	# 'V. Khachatryan' 			: 'Khachatryan, Vardan',
-	# 'S. Chatrchyan' 			: 'Khachatryan, Serguei',
-	'A.M. Sirunyan' 			: 'Sirunyan, Albert'
+  # 'V. Khachatryan'      : 'Khachatryan, Vardan',
+  # 'S. Chatrchyan'       : 'Khachatryan, Serguei',
+  'A.M. Sirunyan'       : 'Sirunyan, Albert'
 }
 
 last_author = {
-	'N. Woods' 						: 'Woods, Nate',
-	# 'J. Swanson' 					: 'Swanson, Joshua',
-	# 'W.H. Smith' 					: 'Smith, Wesley',
+  'N. Woods'            : 'Woods, Nate',
+  # 'J. Swanson'          : 'Swanson, Joshua',
+  # 'W.H. Smith'          : 'Smith, Wesley',
 }
 
 journals = {
-	'JHEP'							: 'Journal of High Energy Physics', 
-	'Phys. Rev. Lett.'	: 'Physical Review Letters', 
-	'Eur. Phys. J.' 		: 'European physical journal', 
-	'JINST' 						: 'Journal of Instrumentation', 
-	'Phys. Lett.' 			: 'Physics letters', 										
-	'Phys. Rev.' 				: 'Physical Review', 
-	'Nature'						: 'Nature Physics'
+  'JHEP'              : 'Journal of High Energy Physics', 
+  'Phys. Rev. Lett.'  : 'Physical Review Letters', 
+  'Eur. Phys. J.'     : 'European physical journal', 
+  'JINST'             : 'Journal of Instrumentation', 
+  'Phys. Lett.'       : 'Physics letters',                    
+  'Phys. Rev.'        : 'Physical Review', 
+  'Nature'            : 'Nature Physics'
 }
 
 issn = {
-	'JHEP' 							: '1029-8479',
-	'Phys. Rev. Lett.'	: '0031-9007',
-	'Eur. Phys. J. C' 	: '1434-6044',
-	'JINST' 						: '1748-0221',
-	'Phys. Lett. B' 		: '0370-2693',
-	'Nature' 						: '1745-2473',
-	'Phys. Rev. C'  		: '0556-2813',
-	'Phys. Rev. D'  		: '1550-7998'
+  'JHEP'              : '1029-8479',
+  'Phys. Rev. Lett.'  : '0031-9007',
+  'Eur. Phys. J. C'   : '1434-6044',
+  'JINST'             : '1748-0221',
+  'Phys. Lett. B'     : '0370-2693',
+  'Nature'            : '1745-2473',
+  'Phys. Rev. C'      : '0556-2813',
+  'Phys. Rev. D'      : '1550-7998'
 }
 
 # --------------------------------------------------
 
 def get_list_of_papers(list_of_papers):
 
-	with open(list_of_papers) as f:
-		temp = f.read()
+  with open(list_of_papers) as f:
+    temp = f.read()
 
-	return bibtexparser.loads(temp)
+  return bibtexparser.loads(temp)
 
 def get_abstract_and_title(arxiv_name):
 
-	r = urllib2.urlopen('http://arxiv.org/abs/' + arxiv_name).read()
+  r = urllib2.urlopen('http://arxiv.org/abs/' + arxiv_name).read()
 
-	soup = BeautifulSoup(r, 'html.parser')
+  soup = BeautifulSoup(r, 'html.parser')
 
-	abstract = ''
-	title = ''
+  abstract = ''
+  title = ''
 
-	for line in soup.find_all('blockquote', {'class':'abstract mathjax'}):
-		abstract += line.text[len('Abstract')+3:]
+  for line in soup.find_all('blockquote', {'class':'abstract mathjax'}):
+    abstract += line.text[len('Abstract')+3:]
 
-	for line in soup.find_all('h1', {'class':'title mathjax'}):
-		title += line.text[len('Title')+2:]
+  for line in soup.find_all('h1', {'class':'title mathjax'}):
+    title += line.text[len('Title')+2:]
 
-	return [abstract, title.replace('  ', ' ') ]
+  return [abstract, title.replace('  ', ' ') ]
 
 def get_journal(name, volume):
-	
-	if name not in journals:
-		return '*** dictionary_journals is missing name!! ***', name
-	
-	else:
-		if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name =='Phys. Rev.':
-			return journals[name] + ' ' + volume[0]
-		else:
-			return journals[name]
+  
+  if name not in journals:
+    return '*** dictionary_journals is missing name!! ***', name
+  
+  else:
+    if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name =='Phys. Rev.':
+      return journals[name] + ' ' + volume[0]
+    else:
+      return journals[name]
 
 def get_issn(name, volume):
-	if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name == 'Phys. Rev.':
-		return issn[ name + ' ' + volume[0] ]
-	else:
-		return issn[ name ]
+  if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name == 'Phys. Rev.':
+    return issn[ name + ' ' + volume[0] ]
+  else:
+    return issn[ name ]
 
 def get_volume(name, volume):
-	if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name == 'Phys. Rev.':
-		return volume[1:]
-	else:
-		return volume
+  if name == 'Eur. Phys. J.' or name == 'Phys. Lett.' or name == 'Phys. Rev.':
+    return volume[1:]
+  else:
+    return volume
 
 def get_url(arxiv_name):
 
-	browser = webdriver.Firefox()
-	browser.get('http://arxiv.org/abs/' + arxiv_name)
+  browser = webdriver.Firefox()
+  browser.get('http://arxiv.org/abs/' + arxiv_name)
 
-	url = ''
+  url = ''
 
-	try:
-		browser.find_element_by_partial_link_text('/').click()
-		url = browser.current_url
-	except:
-		pass
+  try:
+    browser.find_element_by_partial_link_text('/').click()
+    url = browser.current_url
+  except:
+    pass
 
-	browser.quit()
+  browser.quit()
 
-	return url
+  return url
 
 def prepare_input_for_CROSBI(list_of_papers, output_file):
 
-	data = {}
+  data = {}
 
-	# Loop over all papers which are stored in bib
-	for _n, _p in enumerate(list_of_papers.entries):
+  # Loop over all papers which are stored in bib
+  for _n, _p in enumerate(list_of_papers.entries):
 
-		# if _n > 0:
-		# 	break
+    # if _n > 0:
+    #   break
 
-		# If eprint list not empty skip all but those in list
-		if len(eprint) and _p['eprint'] not in eprint:
-			continue
+    # If eprint list not empty skip all but those in list
+    if len(eprint) and _p['eprint'] not in eprint:
+      continue
 
-		# 1. Get the arxiv paper id
-		_eprint = _p['eprint']
+    # 1. Get the arxiv paper id
+    _eprint = _p['eprint']
 
-		# 2. Get abstract and title from arxiv since they dont have bibtex syntax
-		_abstract, _title = get_abstract_and_title(_eprint)
+    # 2. Get abstract and title from arxiv since they dont have bibtex syntax
+    _abstract, _title = get_abstract_and_title(_eprint)
 
-		# 3. DOI
-		_doi = _p['doi']
+    # 3. DOI
+    _doi = _p['doi']
 
-		# 4. Authors
-		# Load pdf
-		_pdf 		= 'pdf/' + _p['eprint'] + '.pdf'
-		_paper 	= convert_pdf_to_txt(_pdf)
+    # 4. Authors
+    # Load pdf
+    _pdf    = 'pdf/' + _p['eprint'] + '.pdf'
+    _paper  = convert_pdf_to_txt(_pdf)
 
-		# First author
-		_authors = first_author.values()[0] + '; ...'
+    # First author
+    _authors = first_author.values()[0] + '; ...'
 
-		# Cro authors
-		for _a, _a_pretty in authors.iteritems():
+    # Cro authors
+    for _a, _a_pretty in authors.iteritems():
 
-			if _a in _paper:
-				_authors += ' ; ' + _a_pretty
+      if _a in _paper:
+        _authors += ' ; ' + _a_pretty
 
-		# Last author
-		_authors += ' ; ... ; ' + last_author.values()[0]
+    # Last author
+    _authors += ' ; ... ; ' + last_author.values()[0]
 
-		# 5. Year
-		_year = _p['year']
+    # 5. Year
+    _year = _p['year']
 
-		# 6. Journal
-		_journal = get_journal( _p['journal'], _p['volume'])
+    # 6. Journal
+    _journal = get_journal( _p['journal'], _p['volume'])
 
-		# 7. ISSN
-		_issn = get_issn( _p['journal'], _p['volume'])
+    # 7. ISSN
+    _issn = get_issn( _p['journal'], _p['volume'])
 
-		# 8. Volume
-		_volume = get_volume( _p['journal'], _p['volume'])
+    # 8. Volume
+    _volume = get_volume( _p['journal'], _p['volume'])
 
-		# 9. Page
-		_page = _p['pages']
+    # 9. Page
+    _page = _p['pages']
 
-		# 10. url
-		_url = get_url(_p['eprint']).rstrip()
+    # 10. url
+    _url = get_url(_p['eprint']).rstrip()
 
-		# Save output
-		_temp = {}
-		_temp['doi'] 			= _doi
-		_temp['autori'] 	= _authors
-		try:
-			_temp['naslov'] = _title
-		except:
-			_temp['naslov'] = _p['title']
-		_temp['godina'] 	= _year
-		_temp['casopis'] 	= _journal
-		_temp['issn'] 		= _issn
-		_temp['volumen'] 	= _volume
-		_temp['stranice'] = _page
-		_temp['sazetak'] 	= _abstract
-		_temp['url'] 			= _url
+    # Save output
+    _temp = {}
+    _temp['doi']      = _doi
+    _temp['autori']   = _authors
+    try:
+      _temp['naslov'] = _title
+    except:
+      _temp['naslov'] = _p['title']
+    _temp['godina']   = _year
+    _temp['casopis']  = _journal
+    _temp['issn']     = _issn
+    _temp['volumen']  = _volume
+    _temp['stranice'] = _page
+    _temp['sazetak']  = _abstract
+    _temp['url']      = _url
 
-		data[_eprint] = _temp
+    data[_eprint] = _temp
 
-		print '\n'
-		print _n
-		print _eprint
-		print _title
-		print _doi
-		print _authors
-		print _year
-		print _journal
-		print _issn
-		print _volume
-		print _page
-		print _abstract
-		print _url
+    print '\n'
+    print _n
+    print _eprint
+    print _title
+    print _doi
+    print _authors
+    print _year
+    print _journal
+    print _issn
+    print _volume
+    print _page
+    print _abstract
+    print _url
 
-	# Output file, i.e. input for CROSBI
-	with open(output_file, 'w') as outfile:
-		json.dump(data, outfile)
+  # Output file, i.e. input for CROSBI
+  with open(output_file, 'w') as outfile:
+    json.dump(data, outfile)
 
 def download_pdfs(list_of_papers):
 
-	print 'Downloading pdfs...'
+  print 'Downloading pdfs...'
 
-	if not os.path.isdir('pdf'):
-		os.mkdir('pdf')
+  if not os.path.isdir('pdf'):
+    os.mkdir('pdf')
 
-	# Loop over all papers which are stored in bib
-	for _n, _p in enumerate(list_of_papers.entries):
+  # Loop over all papers which are stored in bib
+  for _n, _p in enumerate(list_of_papers.entries):
 
-		# If eprint list not empty skip all but those in list
-		if len(eprint) and _p['eprint'] not in eprint:
-			continue
+    # If eprint list not empty skip all but those in list
+    if len(eprint) and _p['eprint'] not in eprint:
+      continue
 
-		_pdf = 'pdf/' + _p['eprint'] + '.pdf'
+    _pdf = 'pdf/' + _p['eprint'] + '.pdf'
 
-		if os.path.isfile(_pdf):
-			print '{0} already exists.'.format(_pdf)
-			continue
+    if os.path.isfile(_pdf):
+      print '{0} already exists.'.format(_pdf)
+      continue
 
-		pageContent = urllib2.urlopen('http://arxiv.org/' + _pdf)
-		file = open( _pdf, 'wb')
-		file.write(pageContent.read())
-		file.close()
+    pageContent = urllib2.urlopen('http://arxiv.org/' + _pdf)
+    file = open( _pdf, 'wb')
+    file.write(pageContent.read())
+    file.close()
 
-		print 'Downloaded {0}'.format(_pdf)
+    print 'Downloaded {0}'.format(_pdf)
 
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
@@ -285,17 +285,17 @@ def convert_pdf_to_txt(path):
     fp.close()
     device.close()
     retstr.close()
-    return text	
+    return text 
 
 # --------------------------------------------------
 
 if __name__ == '__main__':
 
-	# Load list of papers from bib file	
-	list_of_papers = get_list_of_papers('list_of_papers.bib')
+  # Load list of papers from bib file 
+  list_of_papers = get_list_of_papers('list_of_papers.bib')
 
-	# Download pdfs if they don't exist
-	download_pdfs(list_of_papers)
+  # Download pdfs if they don't exist
+  download_pdfs(list_of_papers)
 
-	# Create input for CROSBI, name of output hardcoded in the function
-	prepare_input_for_CROSBI(list_of_papers, output_file)
+  # Create input for CROSBI, name of output hardcoded in the function
+  prepare_input_for_CROSBI(list_of_papers, output_file)
--- readme.md
+++ readme.md
@@ -19,10 +19,11 @@ pip install -r requirements.txt
 2. Get the list of articles by using following command (NOTE: change 2017-> year of interest)
 ``` find a brigljevic and cn cms and date 2017->2017 and ps p ```
 3. Save the output to list_of_papers.bib
-4. _1_prepare_input.py takes list_of_papers.bib as input and gives you json file with all the informations ready for uploading to CROSBI, additionaly all articles are downloaded as pdf from arxiv and stored in pdf directory. 
+4. _1_prepare_input.py takes list_of_papers.bib as input and collects all the information for CROSBI (.json), additionaly all articles are downloaded as pdf from arxiv and stored in pdf directory. 
 ``` python _1_prepare_input.py ```
 
 ### Uploading to CROSBI
-1. Set your username and password in _2_upload_files.py
-2. python _2_upload_files.py
-
+1. Before uploading, change username and password in [_2_upload_files.py](https://github.com/BenjaminMesic/crosbi/blob/master/_2_upload_files.py#L19-L20)
+2. Start uploading
+``` python _2_upload_files.py ```
+3. After step 2 is done, send redni_brojevi_radova.txt to Bojan Macan.
